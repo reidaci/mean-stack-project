b347f0864513e44ab84e49b9dd235b258f1dbc4b
Rei Daci <reiz.daci@outlook.com>
2024-02-15 11:09:02 +0100

    creating a post model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Post } from './posts/post-create/post.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-storedPosts = []
+storedPosts: Post[] = []
 
 onPostAdded(post) {
   this.storedPosts.push(post)
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter,Output } from "@angular/core";
-
+import { Post } from "./post.model";
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
@@ -9,11 +9,11 @@ import { Component, EventEmitter,Output } from "@angular/core";
 export class PostCreateComponent {
     enteredTitle =''
     enteredContent = ''
-  @Output() postCreated = new EventEmitter()
+  @Output() postCreated = new EventEmitter<Post>()
 
     onAddPost() {
 
-        const post = {title: this.enteredTitle 
+        const post: Post = {title: this.enteredTitle 
             , content: this.enteredContent}
             this.postCreated.emit(post)
     }
