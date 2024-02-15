2482485bb50dd7272d3c30e5059d9c3a9645ca9c
Rei Daci <reiz.daci@outlook.com>
2024-02-15 11:00:41 +0100

    add a component for displaying and managing user posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {}
+storedPosts = []
+
+onPostAdded(post) {
+  this.storedPosts.push(post)
+}
+
+
+
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter,Output } from "@angular/core";
 
 @Component({
     selector: 'app-post-create',
@@ -7,13 +7,15 @@ import { Component } from "@angular/core";
 })  
 
 export class PostCreateComponent {
-    enteredValue =''
-    newPost = 'NO CONTENT'
+    enteredTitle =''
+    enteredContent = ''
+  @Output() postCreated = new EventEmitter()
 
     onAddPost() {
-        this.newPost =  this.enteredValue
-        console.log(this.enteredValue);
-        
+
+        const post = {title: this.enteredTitle 
+            , content: this.enteredContent}
+            this.postCreated.emit(post)
     }
 
 
