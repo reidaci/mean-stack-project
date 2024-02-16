66d71d917b4cea568405de1104012f6d7ab44807
Rei Daci <reiz.daci@outlook.com>
2024-02-16 16:31:02 +0100

    connect app to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose")
 
 const Post = require('./models/post')
 const app = express();
+
+mongoose.connect("mongodb+srv://reizAdmin:HvCIpdoJ23ENHwDl@cluster0.m9sdcm8.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+    console.log('Connected to db!');
+}).catch(() => {
+    console.log('Connection failed!');
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -50,5 +57,5 @@ app.get("/api/posts", (req, res, next) => {
     posts: posts
   });
 });
-// HvCIpdoJ23ENHwDl
+
 module.exports = app;
