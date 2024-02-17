7d7121a9b2da4ec04bc8819b4c48f188338b8768
Rei Daci <reiz.daci@outlook.com>
2024-02-17 14:54:46 +0100

    transform response (_id)

--- src/app/posts/post-create/posts.service.ts
+++ src/app/posts/post-create/posts.service.ts
@@ -2,6 +2,7 @@ import { Post } from "./post.model"
 import { Subject } from 'rxjs'
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs";
 
 
 @Injectable({ providedIn: 'root' })
@@ -14,10 +15,20 @@ export class PostsService {
 
 
     getPosts() {
-        this.http.get<{ id: null, message: string, posts: Post[] }>('http://localhost:3000/api/posts').subscribe((postData) => {
-            this.posts = postData.posts
-            this.postsUpdated.next([...this.posts])
-        })
+        this.http.get<{ id: null, message: string, posts: any }>('http://localhost:3000/api/posts')
+            .pipe(map((postData) => {
+                return postData.posts.map(post => {
+                    return {
+                        title: post.title,
+                        content: post.content,
+                        id: post._id
+                    }
+                })
+            }))
+            .subscribe((transformedPosts) => {
+                this.posts = transformedPosts
+                this.postsUpdated.next([...this.posts])
+            })
     }
 
     getPostUpdateListener() {
