99f26eb859ea1b850a60be2f58956fb2eb550789
Rei Daci <reiz.daci@outlook.com>
2024-02-17 15:21:38 +0100

    fix bug ( post delete )

--- backend/app.js
+++ backend/app.js
@@ -52,10 +52,11 @@ app.get("/api/posts", (req, res, next) => {
 
 app.delete("/api/posts/:id", (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
+    res.status(200).json({message: 'Post Deleted'})
     console.log(result);
+    console.log(req.params.id);
   })
-  console.log(req.params.id);
-  res.status(200).json({message: 'Post Deleted'})
+  
 })
 
 
