fe7b415b9af3bfa565e2ff0fda2432f56ea4547c
Rei Daci <reiz.daci@outlook.com>
2024-02-21 12:01:36 +0100

    refactor

--- src/app/posts/post-create/post-list/post-list.component.ts
+++ src/app/posts/post-create/post-list/post-list.component.ts
@@ -1,39 +1,34 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
+
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
-import { Subscription } from "rxjs";
-@Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
-})
 
+@Component({
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
+})
 export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
-  //     {title: 'First Post', content: "This is the fist post's content"},
-  //     {title: 'Second Post', content: "This is the second post's content"},
-  //     {title: 'Third Post', content: "This is the third post's content"}
-  // ]
-
-
-  posts: Post[] = []
-  isLoading = false
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
+  // ];
+  posts: Post[] = [];
+  isLoading = false;
   private postsSub: Subscription;
 
+  constructor(public postsService: PostsService) {}
 
-
-  constructor(public postsService: PostsService) {
-
-  }
   ngOnInit() {
-    this.isLoading = true
-    this.postsService.getPosts()
-
-    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
-      this.isLoading = false
-      this.posts = posts
-
-    })
+    this.isLoading = true;
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.isLoading = false;
+        this.posts = posts;
+      });
   }
 
   onDelete(postId: string) {
@@ -41,7 +36,6 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.postsSub.unsubscribe()
-
+    this.postsSub.unsubscribe();
   }
 }
