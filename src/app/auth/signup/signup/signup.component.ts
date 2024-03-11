d573770a4103ed6c824fa021affb44f0f3a3ae87
Rei Daci <reiz.daci@outlook.com>
2024-03-11 15:32:21 +0100

    add sign up

--- src/app/auth/signup/signup/signup.component.ts
+++ src/app/auth/signup/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+  isLoading = false
+
+  onSignup(form: NgForm) {
+    console.log(form.value);
+    form.reset()
+  }
+
 
 }
