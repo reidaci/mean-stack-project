ac6fff5fd54495d870c0f03ee98aa9d28a25984e
Rei Daci <reiz.daci@outlook.com>
2024-03-11 16:22:15 +0100

    user model (be)

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
-
+const uniqueValidator = require("mongoose-unique-validator")
 
 const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    surname:{type: String , required: true}
-})
+    email: { type: String, required: true, unique: true },
+    password:{type: String , required: true}
+});
 
-module.exports = mongoose.model('Post', postSchema)
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema)
