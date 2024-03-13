8e46fc5e7f65d3eebd5575e713c767cfaa0c8a83
Rei Daci <reiz.daci@outlook.com>
2024-03-13 11:27:57 +0100

    login expire after 1h

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -12,6 +12,7 @@ import { Router } from "@angular/router";
 export class AuthService {
     private isAuthenticated = false
     private token: string;
+    private tokenTimer: any
     private authStatusListener = new Subject<boolean>()
     constructor(private http: HttpClient, private router: Router) {
 
@@ -37,22 +38,27 @@ export class AuthService {
 
     login(email: string, password: string) {
         const authData: AuthData = { email, password }
-        this.http.post<{ token: string }>("http://localhost:3000/api/user/login", authData).subscribe(response => {
+        this.http.post<{ token: string, expiresIn: number }>("http://localhost:3000/api/user/login", authData).subscribe(response => {
             const token = response.token
             this.token = token
             if (token) {
+                const expiresInDuration = response.expiresIn;
+                this.tokenTimer = setTimeout(() => {
+                    this.logout();
+                }, expiresInDuration * 1000)
                 this.isAuthenticated = true
-                this.authStatusListener.next(true)
+                this.authStatusListener.next(true);
                 this.router.navigate(['/']);
             }
 
-        })
+        });
     }
     logout() {
         this.token = null
         this.isAuthenticated = false
         this.authStatusListener.next(false)
-        this.router.navigate(['/'])
+        clearTimeout(this.tokenTimer);
+        this.router.navigate(['/']);
     }
 
 }
