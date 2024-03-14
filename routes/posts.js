cd62ae6fd1e388abd61d3f540e495f2b2ee5cd8a
Rei Daci <reiz.daci@outlook.com>
2024-03-14 16:00:22 +0100

    error messages

--- routes/posts.js
+++ routes/posts.js
@@ -51,7 +51,11 @@ router.post(
           id: createdPost._id
         }
       });
-    });
+    }).catch(error => {
+      res.status(500).json({
+        message: "Creating a post failed"
+      })
+    })
   }
 );
 
@@ -81,6 +85,10 @@ router.put(
       } else {
         res.status(401).json({ message: "Not authorized!" });
       }
+    }).catch(error => {
+      res.status(500).json({
+        message: "Couldn't update post!"
+      })
     });
   }
 );
@@ -104,7 +112,11 @@ router.get("", (req, res, next) => {
         posts: fetchedPosts,
         maxPosts: count
       });
-    });
+    }).catch(error => {
+      res.status.json({
+        message:"Fetching posts failed"
+      })
+    })
 });
 
 router.get("/:id", (req, res, next) => {
@@ -114,6 +126,10 @@ router.get("/:id", (req, res, next) => {
     } else {
       res.status(404).json({ message: "Post not found!" });
     }
+  }).catch(error => {
+    res.status.json({
+      message:"Fetching post failed"
+    })
   });
 });
 
@@ -127,7 +143,11 @@ router.delete("/:id", checkAuth, (req, res, next) => {
         res.status(401).json({ message: "Not authorized!" });
       }
     }
-  );
+  ).catch(error => {
+    res.status.json({
+      message:"Fetching posts failed"
+    })
+  });
 });
 
 module.exports = router;
