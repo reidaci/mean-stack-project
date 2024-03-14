aeffe47a91832efc9ee48cc0a07fe2c458de7da2
Rei Daci <reiz.daci@outlook.com>
2024-03-14 14:38:44 +0100

    handle login&signup errors

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -34,12 +34,12 @@ export class AuthService {
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
-      .post("http://localhost:3000/api/user/signup", authData)
-      .subscribe(response => {
-        console.log(response);
-        this.router.navigate(["/login"]);
+      .post("http://localhost:3000/api/user/signup", authData).subscribe(() => {
+        this.router.navigate["/login"];
+      }, error => {
+        this.authStatusListener.next(false)
+      })
 
-      });
   }
 
   login(email: string, password: string) {
@@ -64,7 +64,9 @@ export class AuthService {
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(["/"]);
         }
-      });
+      }, error => {
+        this.authStatusListener.next(false)
+      })
   }
 
   autoAuthUser() {
--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -1,16 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 import { AuthService } from "../../auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   isLoading = false;
+  private authStatusSub: Subscription
 
   constructor(public authService: AuthService) { }
+
+  ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+      authStatus => {
+        this.isLoading = false
+      }
+    )
+  }
 
   onLogin(form: NgForm) {
     if (form.invalid) {
@@ -19,5 +29,8 @@ export class LoginComponent {
     this.isLoading = true;
     this.authService.login(form.value.email, form.value.password);
     this.isLoading = false;
+  }
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe()
   }
 }
